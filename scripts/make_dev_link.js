102422f75319ef12e9b5447eb9fccb754aa333de
lushan2866 <lushan2866@gmail.com>
2024-11-25 09:49:21 +0800

    0.2

--- scripts/make_dev_link.js
+++ scripts/make_dev_link.js
@@ -10,7 +10,7 @@
 import fs from 'fs';
 import { log, error, getSiYuanDir, chooseTarget, getThisPluginName, makeSymbolicLink } from './utils.js';
 
-let targetDir = 'G:/sy-test/data/plugins';
+let targetDir = 'F:\siyuan-data\data\plugins';
 
 /**
  * 1. Get the parent directory to install the plugin
